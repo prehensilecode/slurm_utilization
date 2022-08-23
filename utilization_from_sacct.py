9c698a314c30ed3b50d5ae4203791a0d427c9732
David Chin <david.w.h.chin@gmail.com>
2022-08-23 12:43:22 -0400

    Update Pipfiles

---
+++ utilization_from_sacct.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+import os
+import pandas
+
+
+def read_sacct(filename):
+    sacct_data = pandas.read_csv(filename, delimiter='|')
+
+    print('Info')
+    sacct_data.info()
+
+    print('')
+
+    print('Description')
+    print(sacct_data.describe())
+
+
+def main():
+    read_sacct('sacct.csv')
+
+
+if __name__ == '__main__':
+    main()
+
